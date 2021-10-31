5517d666d11a13be019b52b68b85c10122e5098b
Anton Zhyvotovskyi <zhyvotovskyi@gmail.com>
2021-10-31 18:57:43 +0200

    update

--- script.js
+++ script.js
@@ -13,47 +13,54 @@ const hiddenHtml = document.querySelector('.container__data-description--hidden'
 // on load
 window.addEventListener('load', () => {
     async function jsonFetch(url) {
-        return fetch(url).then(res => res.json());
+        return fetch(url).then((res) => res.json());
     }
 
     // fetch geolocation data
-    jsonFetch(`https://api.ipdata.co?api-key=${ipdataKey}`).then(responce => {
+    jsonFetch(`https://api.ipdata.co?api-key=${ipdataKey}`).then((responce) => {
         const lon = responce.longitude;
         const lat = responce.latitude;
         const continent = responce.continent_name;
         const country = responce.country_name;
         const city = responce.city;
 
-        cityHtml.textContent = city;
-        countryHtml.textContent = country;
-        continentHtml.textContent = continent;
+        (async () => {
+            cityHtml.textContent = await city;
+            countryHtml.textContent = await country;
+            continentHtml.textContent = await continent;
+        })();
 
         //const proxy = 'https://cors-anywhere.herokuapp.com/';
         //const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openweathermapKey}`;
         const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openweathermapKey}`;
 
         // fetch weather data
-        jsonFetch(api).then(res => {
+        jsonFetch(api).then((res) => {
             const data = res.main;
             const { humidity, pressure } = data;
             const description = res.weather[0].description;
-            const icon = res.weather[0].icon;
             const celc = +(data.temp - 273.15).toFixed(1);
             const feels_like = +(data.feels_like - 273.15).toFixed(1);
-            const iconApi = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+            let iconApi;
+
+            (async () => {
+                const icon = res.weather[0].icon;
+                iconApi = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+            })();
 
             // fetch icon data + description section
-            fetch(iconApi).then(res => {
-                iconHtml.innerHTML = `<img src=${res.url} alt="image" width=\"200px\" />`;
-                degreeHtml.textContent = celc;
-                descriptionHtml.textContent = description;
-                hiddenHtml.innerHTML =
-                    `Feels like: ${feels_like}` +
-                    '<br/>' +
-                    `Humidity: ${humidity}%` +
-                    '<br/>' +
-                    `Pressure: ${pressure}hPa`;
-            });
+            (async () =>
+                await fetch(iconApi).then((res) => {
+                    iconHtml.innerHTML = `<img src=${res.url} alt="image" width=\"200px\" />`;
+                    degreeHtml.textContent = celc;
+                    descriptionHtml.textContent = description;
+                    hiddenHtml.innerHTML =
+                        `Feels like: ${feels_like}` +
+                        '<br/>' +
+                        `Humidity: ${humidity}%` +
+                        '<br/>' +
+                        `Pressure: ${pressure}hPa`;
+                }))();
         });
     });
 });
